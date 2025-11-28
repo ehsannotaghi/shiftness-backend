9d84caa972624ac286ae21b592d43761d77fede1
ehsannotaghi <ehsannotaghi@gmail.com>
2025-11-28 13:10:52 +0000

    400 error fix 6

--- utils/response.php
+++ utils/response.php
@@ -2,7 +2,7 @@
 // Standardized response helper
 // Define sendResponse if not already defined elsewhere
 if (!function_exists('sendResponse')) {
-	function sendResponse($statusCode, $success, $message, $data = null) {
+	function sendResponse($statusCode, $success, $message, $data = null, $error = null) {
 		http_response_code($statusCode);
 		header('Content-Type: application/json; charset=utf-8');
 		$response = [
@@ -11,6 +11,9 @@ if (!function_exists('sendResponse')) {
 		];
 		if ($data !== null) {
 			$response['data'] = $data;
+		}
+		if ($error !== null) {
+			$response['error'] = $error;
 		}
 		echo json_encode($response);
 		exit;
